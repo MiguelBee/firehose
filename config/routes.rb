6b7bb658b02c4556a31591f89546ef324de36ff4
Miguel Bustamante <phatbastard81@yahoo.com>
2017-02-16 17:21:15 +0000

    fixed styles

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :quotes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'about', to: 'quotes#about'
-    get 'contact', to: 'quotes#contact'
+    get 'contact', to: 'quotes#contact' 
+    
 end
